c130b3ecc27cd403dadcee8836211ec74bc936a3
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-01-28 18:19:55 +0800

    update trainer member

--- database/migrations/2023_01_28_034222_create_members_table.php
+++ database/migrations/2023_01_28_034222_create_members_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->date('membership_expiration');
+            $table->foreignId('trainer_id')->constrained();
             $table->timestamps();
         });
     }
